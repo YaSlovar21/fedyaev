388b8a956c356ce27253a4ba003db27cd5d0c959
YaSlovar_work <stanko.tsep@yandex.ru>
2023-11-29 16:23:37 +0700

    start

---
+++ sitemap.js
@@ -0,0 +1,569 @@
+
+const dateNow = (new Date()).toString();
+
+module.exports.paths = [
+    {
+      path: '/',
+      lastmod: dateNow,
+      priority: 1,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/production.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/harakteristiki-plastinchatogo-teploobmennika.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/promyvka-teploobmennika.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/teploizolyaciya.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/plastina-plastinchatogo-teploobmennika.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/uplotnenie-plastinchatogo-teploobmennika.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/princip-raboty-i-ustrojstvo.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/montazh-teploobmennika.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/remont-plastinchatogo-teploobmennika.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/plastinchatye-teploobmenniki/programma-rascheta.html',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/pishchevye-teploobmenniki.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blochnye-teplovye-punkty/',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/pasterizatsionno-okhladitelnye-ustanovki.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/service.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/about/',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/about/vakansii/',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/about/vakansii/vakansiya-inzhenera-konstruktora-barnaul.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/contacts.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/equipment-selection/',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/',
+      lastmod: dateNow,
+      priority: 0.9,
+      changefreq: 'monthly'
+    },
+
+    /* блог */
+    {
+      path: '/blog-proizvodstva/',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    /* статьи */
+    {
+      path: '/blog-proizvodstva/plastinchatye-teploobmenniki-otopleniya.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'weekly'
+    },
+    {
+      path: '/blog-proizvodstva/plastinchatye-teploobmenniki-gvs.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'weekly'
+    },
+    {
+      path: '/blog-proizvodstva/plastinchatyy-pasterizator.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'weekly'
+    },
+    {
+      path: '/blog-proizvodstva/mnogosektsionnye-teploobmenniki.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/preimushchestva-plastinchatykh-teploobmennikov.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/plastinchatii-teploobmennik-du-250.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmennik-5-mvt-object-spb.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/plastinchatye-ohladiteli-ool.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/plastinchatii-ohladitel-ool-10.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/proektirovanie-blochnykh-teplovykh-punktov.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmenniki-otopleniya-chastnogo-doma.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmenniki-mnogokvartirnogo-doma.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmennik-dlya-bassejna.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/oprosnyj-list-dlya-plastinchatogo-teploobmennika.html',
+      lastmod: dateNow,
+      priority: 0.8,
+      changefreq: 'monthly'
+    },
+    /* статьи - объекты */
+    {
+      path: '/blog-proizvodstva/teploobmenniki-gvs-object-u-timashevsk.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmenniki-gvs-object-p-zaton-vostok.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmenniki-gvs-object-p-umnii-dom.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmennik-par-voda-plastinchatyy.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/blog-proizvodstva/teploobmennik-glikol-voda.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+{
+      path: '/blog-proizvodstva/teploobmennik-dlya-sistemy-snegotayaniya.html',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    
+    /* --- теплообменники --- */
+    {
+      path: '/catalog/ti-025.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-077.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-13.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-18.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-16-5.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-28.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-45.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-65.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-52.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-82.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-95.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/ti-116.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+
+    /* теплообменники пищевые */
+    {
+      path: '/catalog/p-025.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-077.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-13.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-18.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-16-5.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-28.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-45.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-65.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-52.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/catalog/p-82.html',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+
+    /* документы */
+    {
+      path: '/docu/termoblok_catalog.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/docu/Termoblok_Sout.pdf',
+      lastmod: dateNow,
+      priority: 0.5,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/docu/Katalozhnii_list_BTP_Termoblok.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/docu/btp-certificate.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/docu/Oprosnii_list_podbora_teplovogo_punkta.doc',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/docu/Oprosnii_list_podbora_teplovogo_punkta.doc',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+    },
+    {
+      path: '/docu/Termoblok_reference_teploobmenniki.pdf',
+      lastmod: dateNow,
+      priority: 0.7,
+      changefreq: 'monthly'
+    },
+    /* ГЕНЕРАЦИЯ MACE */
+    {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-102-кВт-вентиляция.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-122-кВт-вентиляция.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-1289-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-130-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-137-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-1695-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-1695-кВт-узел-ввода.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-1752-кВт-узел-ввода.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-20-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-207-кВт-вентиляция.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-2352-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-2352-кВт-узел-ввода.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-275-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-276-кВт-узел-ввода.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-337-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-436-кВт-узел-ввода.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-462-кВт-гвс-горячее-водоснабжение.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-520-кВт-гвс-горячее-водоснабжение.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-61-кВт-на-отопление.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     {
+      path: '/blochnye-teplovye-punkty/Блочный-тепловой-пункт-788-кВт-узел-ввода.pdf',
+      lastmod: dateNow,
+      priority: 0.6,
+      changefreq: 'monthly'
+      },
+     
+];
